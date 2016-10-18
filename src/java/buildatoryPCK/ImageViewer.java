9f7e96777270d54bc1359d28ef1a36e11a274542
Alexandra <alexandracatesson@gmail.com>
2016-10-18 12:06:10 +0200

    18/10/2016 : Debug de l'outil gallerie

--- src/java/buildatoryPCK/ImageViewer.java
+++ src/java/buildatoryPCK/ImageViewer.java
@@ -10,7 +10,8 @@
     import javax.annotation.PostConstruct ;
     import javax.faces.bean.ManagedBean ;
 
-    @ManagedBean
+    
+    @ManagedBean(name="ImageViewer")
     public class ImageViewer {
 
         private List<String> images;
@@ -18,12 +19,16 @@
         @PostConstruct
         public void init() {
             images = new ArrayList <String>();
-            images.add("108142.jpg");
-            images.add("144565.jpg");
-            images.add("57497.jpg");
-            images.add("5cl3l0W.jpg");
+                for(int i=1;i<=4;i++) {  
+                images.add("Manga"+i+".jpg");
+            }
         }
 
+            //images.add("108142.jpg");
+            //images.add("144565.jpg");
+            //images.add("57497.jpg");
+            //images.add("5cl3l0W.jpg");
+        
         public List<String> getImages() {
             return images;
         }
