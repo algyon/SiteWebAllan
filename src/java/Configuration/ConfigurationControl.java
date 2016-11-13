be66e5461930de99de64c96f1b43b04fefe2f18a
algyo <algyo@DESKTOP-JJCTGV1>
2016-11-13 20:14:37 +0100

    Commit Page Fonctionnelle choice & Build

--- src/java/Configuration/ConfigurationControl.java
+++ src/java/Configuration/ConfigurationControl.java
@@ -15,20 +15,13 @@ import Ram.RamDao;
 import Stockage.StockageDao;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -59,10 +52,7 @@ public class ConfigurationControl implements Serializable {
     private ConfigurationDao ConfigurationDao;
     private Configuration ConfigurationSaisie;
     private Configuration SelectedConfig;
-    @PersistenceContext(unitName = "SiteWebAllanPU")
-    private EntityManager em;
-    @Resource
-    private javax.transaction.UserTransaction utx;
+    private float PrixSelectedConfig;
 
     /**
      * Creates a new instance of ConfigurationControl
@@ -107,12 +97,21 @@ public class ConfigurationControl implements Serializable {
     public Configuration getSelectedConfig() {
         if (SelectedConfig.getNomConfig() == null) {
             SelectedConfig = ConfigurationDao.getConfigurationByNom(getNomConfig());
+            this.setPrixBuild();
         }
         return SelectedConfig;
     }
 
     public void setSelectedConfig(Configuration SelectedConfig) {
         this.SelectedConfig = SelectedConfig;
+    }
+
+    public float getPrixSelectedConfig() {
+        return PrixSelectedConfig;
+    }
+
+    public void setPrixSelectedConfig(float PrixSelectedConfig) {
+        this.PrixSelectedConfig = PrixSelectedConfig;
     }
 
     public String getParamValueNameConfig(FacesContext fc) {
@@ -129,6 +128,7 @@ public class ConfigurationControl implements Serializable {
     public void cpuValueChanged() {
         try {
             SelectedConfig.setNomCpu(CpuDao.getCpuByNom(SelectedConfig.getNomCpu().getNomCpu()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -137,6 +137,7 @@ public class ConfigurationControl implements Serializable {
     public void cmValueChanged() {
         try {
             SelectedConfig.setNomCm(CmDao.getCMByNom(SelectedConfig.getNomCm().getNomCm()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -145,6 +146,7 @@ public class ConfigurationControl implements Serializable {
     public void cgValueChanged() {
         try {
             SelectedConfig.setNomCg(CgDao.getCgByNom(SelectedConfig.getNomCg().getNomCg()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -153,6 +155,7 @@ public class ConfigurationControl implements Serializable {
     public void stockageValueChanged() {
         try {
             SelectedConfig.setNomStockage(StockageDao.getStockageByNom(SelectedConfig.getNomStockage().getNomStockage()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -161,6 +164,7 @@ public class ConfigurationControl implements Serializable {
     public void ramValueChanged() {
         try {
             SelectedConfig.setNomRam(RamDao.getRamByNom(SelectedConfig.getNomRam().getNomRam()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -169,6 +173,7 @@ public class ConfigurationControl implements Serializable {
     public void coolingValueChanged() {
         try {
             SelectedConfig.setNomCooling(CoolingDao.getCoolingByNom(SelectedConfig.getNomCooling().getNomCooling()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -177,6 +182,7 @@ public class ConfigurationControl implements Serializable {
     public void alimentationValueChanged() {
         try {
             SelectedConfig.setNomAlim(AlimDao.getAlimByNom(SelectedConfig.getNomAlim().getNomAlim()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -185,6 +191,7 @@ public class ConfigurationControl implements Serializable {
     public void boitierValueChanged() {
         try {
             SelectedConfig.setNomBoitier(BoitierDao.getBoitierByNom(SelectedConfig.getNomBoitier().getNomBoitier()));
+            this.setPrixBuild();
         } catch (NullPointerException f) {
             System.err.println(f);
         }
@@ -208,19 +215,41 @@ public class ConfigurationControl implements Serializable {
         float Prixcalc = 0;
         String StringPrixCalc;
 
-        if (nomElement == "stockage"){
-            Prixcalc = SelectedConfig.getNomStockage().getPrix() - prixSelect;
-        } else if (nomElement == "cpu") {
-            Prixcalc = SelectedConfig.getNomCpu().getPrix() - prixSelect;
-            System.out.println("ICI YO");
-        } else if (nomElement == "cm"){
-            Prixcalc = SelectedConfig.getNomCm().getPrix() - prixSelect;
+        if (null != nomElement) switch (nomElement) {
+            case "stockage":
+                Prixcalc = prixSelect - this.getSelectedConfig().getNomStockage().getPrix();
+                break;
+            case "cpu":
+                Prixcalc = prixSelect - this.getSelectedConfig().getNomCpu().getPrix();
+                break;
+            case "cm":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomCm().getPrix();
+                break;
+            case "cg":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomCg().getPrix();
+                break;
+            case "alim":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomAlim().getPrix();
+                break;
+            case "boitier":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomBoitier().getPrix();
+                break;
+            case "ram":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomRam().getPrix();
+                break;
+            case "cooling":
+                Prixcalc = prixSelect-this.getSelectedConfig().getNomCooling().getPrix();
+                break;
+            default:
+                break;
         }
-        
-        if (Prixcalc < 0) {
-            StringPrixCalc = "+" + BigDecimal.valueOf(Prixcalc).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue();
+
+        if (Prixcalc < 0.0) {
+            StringPrixCalc = " " + BigDecimal.valueOf(Prixcalc).setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
+        } else if (Prixcalc == 0.0){
+            StringPrixCalc = "0.0";
         } else {
-            StringPrixCalc = "-" + BigDecimal.valueOf(Prixcalc).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue();
+            StringPrixCalc = " +" + BigDecimal.valueOf(Prixcalc).setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
         }
 
         return StringPrixCalc;
@@ -236,12 +265,32 @@ public class ConfigurationControl implements Serializable {
                     + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomRam().getPrix()
                     + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomStockage().getPrix()
                     + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomBoitier().getPrix()
-                    + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomAlim().getPrix();
+                    + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomAlim().getPrix()
+                    + this.ConfigurationDao.getConfigurationByNom(nomConfig).getNomCooling().getPrix();
+        } catch (NullPointerException e) {
+            e.getMessage();
+        }
+
+        return BigDecimal.valueOf(PrixConfig).setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
+    }
+
+    public void setPrixBuild() {
+        float PrixConfig;
+        PrixConfig = 0;
+        System.out.println("Je suis bien appellé");
+        try {
+            PrixConfig = SelectedConfig.getNomAlim().getPrix()
+                    + SelectedConfig.getNomCpu().getPrix()
+                    + SelectedConfig.getNomCg().getPrix()
+                    + SelectedConfig.getNomCm().getPrix()
+                    + SelectedConfig.getNomRam().getPrix()
+                    + SelectedConfig.getNomCooling().getPrix()
+                    + SelectedConfig.getNomBoitier().getPrix()
+                    + SelectedConfig.getNomStockage().getPrix();
         } catch (NullPointerException e) {
             e.getMessage();
             System.out.println("NIQUE TA MERE");
         }
-
-        return BigDecimal.valueOf(PrixConfig).setScale(2,BigDecimal.ROUND_HALF_UP).floatValue();
+        this.setPrixSelectedConfig(BigDecimal.valueOf(PrixConfig).setScale(2, BigDecimal.ROUND_HALF_UP).floatValue());
     }
 }
