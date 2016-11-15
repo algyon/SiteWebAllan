f87fa726bc07216799ad21411acb07209bc44ad6
algyo <algyo@DESKTOP-JJCTGV1>
2016-11-15 22:18:31 +0100

    Finalisation du coté utilisateur

--- src/java/Commande/CommandeControl.java
+++ src/java/Commande/CommandeControl.java
@@ -8,6 +8,9 @@ package Commande;
 import Configuration.Configuration;
 import Configuration.ConfigurationControl;
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
@@ -25,20 +28,18 @@ public class CommandeControl implements Serializable {
 
     @EJB
     private CommandeDao CommandeDao;
+
     private Commande CommandeSaisie;
     private ConfigurationControl ConfigurationControl;
+
     private boolean CommandeExpe;
     private boolean CommandeMontage;
+    private float PrixTot;
 
     public CommandeControl() {
         CommandeSaisie = new Commande();
     }
 
-    /*
-    @PostConstruct
-    public void init() {
-        SelectedCM = CarteMereDao.getCMByNom("Asus MAXIMUS VIII RANGER");
-    }*/
     public List<Commande> getAllCommande() {
         return CommandeDao.getAllCommande();
     }
@@ -51,23 +52,44 @@ public class CommandeControl implements Serializable {
         this.CommandeSaisie = Commande;
     }
 
-    public String save() {
-        Configuration config;
-        config = ConfigurationControl.getSelectedConfig();
-        
+    public String calcPrixTotal(float prix) {
+        if (CommandeSaisie.getExpedition()) {
+            prix += 15.0;
+        }
+
+        if (CommandeSaisie.getMontage()) {
+            prix += 30.0;
+        }
+        this.setPrixTot(prix);
+        return "Paiement";
+    }
+
+    public String save(Configuration config) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String dateString;
+        dateString = sdf.format(date);
+        try {
+            date = sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        CommandeSaisie.setNomCpu(config.getNomCpu().getNomCpu());
+        CommandeSaisie.setNomCm(config.getNomCm().getNomCm());
+        CommandeSaisie.setNomCg(config.getNomCg().getNomCg());
+        CommandeSaisie.setNomRam(config.getNomRam().getNomRam());
+        CommandeSaisie.setNomStockage(config.getNomStockage().getNomStockage());
+        CommandeSaisie.setNomCooling(config.getNomCooling().getNomCooling());
+        CommandeSaisie.setNomBoitier(config.getNomBoitier().getNomBoitier());
+        CommandeSaisie.setNomAlim(config.getNomAlim().getNomAlim());
+        CommandeSaisie.setDateCommande(date);
+        CommandeSaisie.setPrixTotal(PrixTot);
+
         CommandeDao.save(CommandeSaisie);
+
         return "Welcome";
     }
-    
-    /*
-    public void saveAltCommande() {
-        ConfigurationControl configuration;
-        ConfigurationDao ConfigurationDao;
-        configuration = ConfigurationDao.
-        
-        CommandeSaisie.setNomAlim(configuration.getSelectedConfig());
-        CommandeDao.save(CommandeSaisie);
-    }*/
 
     public String delete() {
         CommandeDao.delete(CommandeSaisie);
@@ -78,7 +100,7 @@ public class CommandeControl implements Serializable {
         CommandeDao.update(CommandeSaisie);
         return "liste";
     }
-    
+
     public void lireCommande(ComponentSystemEvent event) {
         CommandeSaisie = CommandeDao.getCommande();
     }
@@ -106,33 +128,33 @@ public class CommandeControl implements Serializable {
     public void setCommandeExpe(boolean CommandeExpe) {
         this.CommandeExpe = CommandeExpe;
     }
-    
-    public String getLivraisonToString () {
+
+    public String getLivraisonToString() {
         String livraisonToString;
-        if(CommandeSaisie.getExpedition() == true){
-            livraisonToString= "Livraison ";
+        if (CommandeSaisie.getExpedition() == true) {
+            livraisonToString = "Livraison ";
         } else {
-            livraisonToString =" Pas de Livraison ";
+            livraisonToString = " Pas de Livraison ";
         }
         return livraisonToString;
     }
-    
+
     public String getMontageToString() {
         String MontageToString;
-        if (CommandeSaisie.getMontage() == true){
+        if (CommandeSaisie.getMontage() == true) {
             MontageToString = "Montage de la configuration";
         } else {
-            MontageToString = "Pas de montage de la confifuration";
+            MontageToString = "Pas de montage de la configuration";
         }
         return MontageToString;
     }
-    
-    public String getCpuInCommande (){
-        String CpuInCommande;
-        // Set 
-        CpuInCommande = ConfigurationControl.getSelectedConfig().getNomCpu().getNomCpu();
-        
-        return CpuInCommande;
+
+    public float getPrixTot() {
+        return PrixTot;
     }
-    
+
+    public void setPrixTot(float PrixTot) {
+        this.PrixTot = PrixTot;
+    }
+
 }
